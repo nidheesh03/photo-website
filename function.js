bb0df843d543d665e83127312c7c8d517424849a
nidheesh03 <nidheeshrocks03@gmail.com>
2020-07-26 11:24:40 +0530

    hey

---
+++ function.js
@@ -0,0 +1,9 @@
+function like(event){
+    var parent=event.target.parentNode;
+    var image=parent.querySelector("img");
+    var x=document.createElement("div");
+    x.className="img-div";
+    x.innerHTML=`<img src="${img.src}" alt="...">`
+    var y=document.getElementsByClassName("function-div");
+    y.appendChild(x);
+}
