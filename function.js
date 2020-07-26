b61b351d74e7fd4003c0acec9e436961d5823747
nidheesh03 <nidheeshrocks03@gmail.com>
2020-07-26 22:13:20 +0530

    hi

--- function.js
+++ function.js
@@ -1,6 +1,5 @@
 function like(event){
     var parent=event.target.parentNode;
-    var z=parent.querySelector("img");
     var x=document.createElement("div");
     x.className="img-div";
     x.innerHTML=`<img src="${img.src}" alt="...">`
