5d811f89de7aadf9d5a290b3725345e30e648c50
nidheesh03 <nidheeshrocks03@gmail.com>
2020-07-27 18:51:29 +0530

    added

--- function.js
+++ function.js
@@ -3,7 +3,12 @@ function like(event){
     var x=document.createElement("div");
     var image=parent.querySelector("img");
     x.className="img-div";
-    x.innerHTML=`<img src="${image.src}" alt="...">`;
+    x.innerHTML=`<img src="${image.src}" alt="..."> <br><button  onclick="dislike(event)">X
+</button>`;
     var remote=document.getElementById("function-div");
     remote.appendChild(x);
+}
+function dislike(event){
+    var x=event.target.parentNode;
+    x.remove();
 }
